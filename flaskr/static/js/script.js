7220bb57911de598f191d18f4c79fe1cd74e65ab
cagedev <cagedev@gmail.com>
2022-06-11 14:27:49 +0200

    Added icon bar (ugly...).

--- flaskr/static/js/script.js
+++ flaskr/static/js/script.js
@@ -1,15 +1,22 @@
 
-// fullscreen toggle
-window.addEventListener('click',
-    () => {
-        if (!document.fullscreenElement) {
-            document.documentElement.requestFullscreen()
-                .then(() => { })
-                .catch(err => {
-                    alert(`An error occurred while trying to switch into fullscreen mode: ${err.message} (${err.name})`);
-                });
-        } else {
-            document.exitFullscreen();
-        }
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen()
+            .then(() => { })
+            .catch(err => {
+                console.log(`An error occurred while trying to switch into fullscreen mode: ${err.message} (${err.name})`);
+            });
+    } else {
+        document.exitFullscreen();
     }
-);
+}
+
+
+// set event listeners
+window.addEventListener('load', () => { 
+    try {
+        document.getElementById('fs-toggle').addEventListener('click', toggleFullscreen);
+    } catch (e) {
+        console.log(e);
+    }
+});
